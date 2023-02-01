11362446eca20739ff725e2a816100f8fe815e2e
“krasowski-alina” <alina.krasowski.web@gmail.com>
2023-02-01 15:10:05 -0500

    Created a function allNewDice for generating an array with random 10 numbers(between 1-6),set the initial state to our function,created a button which change the  state, so every time clicking it generate a new array with numbers. Rendered those 10  numbers on a page by using map method, changed dice array with numbers to  array with objects with 2 values: random number and boolean isHeld: false.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 
+import { useState } from 'react';
 import './App.css';
 import Die from './Die';
 
 function App() {
+
+  const allNewDice = () => {
+    const randomNumbers = [];
+    for(let i =0; i < 10; i++){
+      randomNumbers.push({
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false
+        })
+    }
+    return randomNumbers
+  }
+
+  const [ dice, setDice] = useState(allNewDice())
+
+  const rollDice = () => {
+    setDice(allNewDice())
+  }
   return (
     <main>
       <div className="dice-container">
-        <Die value={1}/>
-        <Die value={2}/>
-        <Die value={3}/>
-        <Die value={4}/>
-        <Die value={5}/>
-        <Die value={6}/>
-        <Die value={7}/>
-        <Die value={8}/>
-        <Die value={9}/>
-        <Die value={10}/>
+        {dice.map((die, index) => (
+          <Die value={die.value} key={index}/>
+        ))}
       </div>
+      <button onClick={rollDice}
+      className="roll-dice">
+        Roll
+      </button>
     </main>
   );
 }
