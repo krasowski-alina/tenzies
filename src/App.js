79d96ff8d3ef952ff346dfeb62fc139692f33eba
“krasowski-alina” <alina.krasowski.web@gmail.com>
2023-02-02 16:29:15 -0500

    installed nanoid dependancy to assign a unique ID to each object in a dice array, added conditional styling to the Die component,created holdDice function to flip isHeld property in the object and to change color on clicked Die component

--- src/App.js
+++ src/App.js
@@ -1,35 +1,50 @@
 
+import { nanoid } from 'nanoid';
 import { useState } from 'react';
+
 import './App.css';
 import Die from './Die';
 
+
 function App() {
-
-  const allNewDice = () => {
-    const randomNumbers = [];
-    for(let i =0; i < 10; i++){
-      randomNumbers.push({
-        value: Math.ceil(Math.random() * 6),
-        isHeld: false
-        })
-    }
-    return randomNumbers
-  }
-
   const [ dice, setDice] = useState(allNewDice())
 
+  function allNewDice() {
+    const randomNumbers = [];
+      for(let i =0; i < 10; i++){
+        randomNumbers.push({
+          value: Math.ceil(Math.random() * 6),
+          isHeld: false,
+          id: nanoid(3)
+        })
+      }
+    return randomNumbers
+  }
   const rollDice = () => {
     setDice(allNewDice())
   }
+  const holdDice = (id) => {
+    setDice(prevDice => prevDice.map(die => {
+      return die.id === id ?
+            {...die, isHeld: !die.isHeld} :
+            die
+    }))
+  }
   return (
     <main>
-      <div className="dice-container">
-        {dice.map((die, index) => (
-          <Die value={die.value} key={index}/>
-        ))}
-      </div>
+      <h1 className="title">Tenzies</h1>
+        <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+          <div className="dice-container">
+            {dice.map((die) => (
+                <Die 
+                value = { die.value }
+                isHeld = { die.isHeld }
+                holdDice = { ()=> holdDice(die.id) }
+                key  = { die.id }/>
+            ))}
+        </div>
       <button onClick={rollDice}
-      className="roll-dice">
+        className="roll-dice">
         Roll
       </button>
     </main>
--- src/Die.js
+++ src/Die.js
@@ -1,7 +1,13 @@
-
-export default function Die({value}){
+export default function Die({value, isHeld, holdDice}){
+    const style = {
+        backgroundColor: isHeld? "#59E391" : "white"
+    }
     return(
-    <div className="die-face">
+    <div 
+        className="die-face" 
+        style={style}
+        onClick={holdDice} 
+        >
         <h2 className="die-num">{value}</h2>
     </div>
     )
