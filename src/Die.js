badabb1387a5de368de4f9f57b3d8821956ecd6c
“krasowski-alina” <alina.krasowski.web@gmail.com>
2023-02-15 10:51:51 -0500

    added real dots on dice, instead of numbers

--- src/Die.js
+++ src/Die.js
@@ -1,15 +1,29 @@
+import one from "./images/1.png"
+import two from "./images/2.png"
+import three from "./images/3.png"
+import four from "./images/4.png"
+import five from "./images/5.png"
+import six from "./images/6.png"
 export default function Die({value, isHeld, holdDice}){
     const style = {
         backgroundColor: isHeld? "#8AC5DC" : "white",
     }
-
+    const images = {1: one,
+                    2: two,
+                    3: three,
+                    4: four,
+                    5: five,
+                    6: six 
+                }
     return(
     <div 
         className="die-face" 
         style = {style}
         onClick = {holdDice} 
         >
-        <h2 className="die-num">{value}</h2>
+        <h2 className="die-num">
+            <img className="dots" src={images[value]} alt="dice"/>
+        </h2>
     </div>
     )
 
